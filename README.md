6fe8068a31aa44d04cb1cc979abda11c312b1b09
ChamodKalupahana <75031974+ChamodKalupahana@users.noreply.github.com>
2022-10-31 18:03:52 +0000

    Finished Notes 1.1 and started Notes 1.2

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # simulating_the_universe_using_GANs
  4th year project, all year long, 50 credits
+supervisor: Adam Moss, Micheal Merrifield
---
+++ test_interpreter.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+start_time = time.time()
+
+print('Hello World')
+
+# measure time taken to execute code (uni interpreter is usually faster than uni_2_1)
+end_time = time.time()
+time_taken = end_time - start_time
+print('Time taken =', str(time_taken) + 'secs')
